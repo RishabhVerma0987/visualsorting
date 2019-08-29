59ba0c0cd2e0e711e6b11d5b7a07dba911adca37
rishabh verma <vermarishabh0987@gmail.com>
2019-08-29 18:25:31 +0530

    made simple UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PrettoSlider from "./components/pretto";
+class App extends React.Component {
+  state = {
+    heights: [130, 220, 120, 150, 80],
+    numberOfElements: 30
+  };
+  handleChange = (event, newValue) => {
+    this.setState({ numberOfElements: newValue });
+    this.addelementsTolist();
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  divStyle = height => {
+    var h = height.toString() + "px";
+    return {
+      backgroundColor: "#e0dda4",
+      borderBottomLeftRadius: "5px",
+      borderBottomRightRadius: "5px",
+      height: h,
+      fontSize: "8px",
+      marginRight: "7px",
+      flex: 1
+    };
+  };
+
+  addBars = () => {
+    var barsList = [];
+
+    for (let i = 0; i < this.state.heights.length; i++) {
+      barsList.push(
+        <div
+          className="bar"
+          style={this.divStyle(this.state.heights[i])}
+          key={i}
+        ></div>
+      );
+    }
+    return barsList;
+  };
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+  addelementsTolist = () => {
+    //less than 250 //more than 2
+    var i = 0;
+    var element = 0;
+    var elementList = [];
+    while (i < this.state.numberOfElements) {
+      element = this.getRandomInt(10, 230);
+      elementList.push(element);
+      i = i + 1;
+    }
+    this.setState({
+      heights: elementList
+    });
+  };
+
+  render() {
+    console.log(this.state.numberOfElements);
+    console.log(this.state.heights);
+    return (
+      <div className="container-fluid">
+        <div className="container1">{this.addBars()}</div>
+        <div className="container2">
+          <div className="row">
+            <PrettoSlider
+              valueLabelDisplay="auto"
+              aria-label="pretto slider"
+              defaultValue={30}
+              max="55"
+              min="2"
+              onChange={this.handleChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/pretto.js
@@ -0,0 +1,33 @@
+import { withStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+const PrettoSlider = withStyles({
+  root: {
+    color: "#52af77",
+    height: 6
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus,&:hover,&$active": {
+      boxShadow: "inherit"
+    }
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)"
+  },
+  track: {
+    height: 6,
+    borderRadius: 4
+  },
+  rail: {
+    height: 6,
+    borderRadius: 4
+  }
+})(Slider);
+
+export default PrettoSlider;
